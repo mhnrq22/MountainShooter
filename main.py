8be24d4476bf605d5d04d69241f5e10ad5377bdc
Moises <moises.acer22@gmail.com>
2026-05-26 20:11:21 -0300

    Adding classes generated from StarUML

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print('Quitting...')
-
             pygame.quit() # Close Window
             quit() # End pygame
 
